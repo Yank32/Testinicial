31cecd6659df98dbe1cfb5b8c218032e47e2df34
Yank32 <67502426+Yank32@users.noreply.github.com>
2020-07-12 19:06:10 -0300

    Update helpstaff.js

--- commands/helpstaff.js
+++ commands/helpstaff.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args) => {
             command = client.commands.cache.get(command);
             var SHembed = new Discord.MessageEmbed()
             .setColor("cyan")
-            .setAuthor(`REDE ENDER`, message.guild.iconURL)
+            .setAuthor(`AetherMC`, message.guild.iconURL)
             .setThumbnail(client.user.displayAvatarURL)
             .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n**Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
             message.channel.send(SHembed);
@@ -26,7 +26,7 @@ module.exports.run = async (client, message, args) => {
         message.delete();
         
         let embed = new Discord.MessageEmbed()
-        .setAuthor(`🌀Comandos Dos Staffs Rede Ender!🌀`, message.guild.iconURL)
+        .setAuthor(`🌀Comandos Dos Staffs AetherMC!🌀`, message.guild.iconURL)
         .setColor("redlight")
         .setImage("https://i.imgur.com/uNW0Sut.png")
         .setDescription(`${message.author.username} ***Mandei no seu privado <3!***`)
@@ -34,16 +34,16 @@ module.exports.run = async (client, message, args) => {
         let Sembed = new Discord.MessageEmbed()
         .setImage("https://i.imgur.com/uNW0Sut.png")
         .setColor("cyan")
-        .setAuthor(`🌠Rede Ender🌠`, message.guild.iconURL)
+        .setAuthor(`🌠AetherMC🌠`, message.guild.iconURL)
         .setThumbnail(client.user.displayAvatarURL)
         .setTimestamp()
         .setDescription(`***Esses são meus comandos atuais!***\n**Meu prefix é: r!**`)
-        .addField(`Comando#1:`, "``-Ban <@usuario> <motivo>``")
-        .addField(`Comando#2:`, "``-Kick <@usuario> <motivo>``")
-        .addField(`Comando#3:`, "``-Limpar <1 a 99> (para limpar mensagens)``")
-        .addField(`Comando#4:`, "``-Anuncio (siga os passos)``")
-        .addField(`Comando#5:`, "``-Aviso <@usuario> <msg de aviso para o usuario>``")
-        .setFooter("REDE ENDER", client.user.displayAvatarURL)
+        .addField(`Comando#1:`, "``a!Ban <@usuario> <motivo>``")
+        .addField(`Comando#2:`, "``a!Kick <@usuario> <motivo>``")
+        .addField(`Comando#3:`, "``a!Limpar <1 a 99> (para limpar mensagens)``")
+        .addField(`Comando#4:`, "``a!Anuncio (siga os passos)``")
+        .addField(`Comando#5:`, "``a!Aviso <@usuario> <msg de aviso para o usuario>``")
+        .setFooter("AetherMC", client.user.displayAvatarURL)
         message.channel.send(embed).then(m => m.delete(10000));
         message.author.send(Sembed)
     }
